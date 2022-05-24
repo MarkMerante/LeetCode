7f53b6265c0ceb31429beadb08b147e144860cb0
MarkMerante <57273536+MarkMerante@users.noreply.github.com>
2022-05-23 21:56:23 -0400

    Add files via upload

---
+++ 36. Valid Sudoku.py
@@ -0,0 +1,16 @@
+# 36. Valid Sudoku
+
+# Determine if a 9 x 9 Sudoku board is valid. Only the filled cells need to be validated according to the following rules:
+# 1. Each row must contain the digits 1-9 without repetition.
+# 2. Each column must contain the digits 1-9 without repetition.
+# 3. Each of the nine 3 x 3 sub-boxes of the grid must contain the digits 1-9 without repetition.
+
+#Notes:
+# A Sudoku board (partially filled) could be valid but is not necessarily solvable.
+# Only the filled cells need to be validated according to the mentioned rules.
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        #Did not code
+        #Referred to the solution instead.
+        #I will conquer this question next time.
