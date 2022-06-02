402ab465df4b5f9fc130f0d8f7ab0e54aefbbee3
MarkMerante <57273536+MarkMerante@users.noreply.github.com>
2022-06-01 21:58:52 -0400

    Update 1. Two Sum.py

--- 1. Two Sum.py
+++ 1. Two Sum.py
@@ -4,16 +4,10 @@
 
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        sortedNums = sorted(nums)
-        newList = []
-        for i in sortedNums:
-            if target <=  i:
-                newList.append(i)
-                
-        n = 1
-        while n < len(newList)+1:
-            search = newList[-n] - target
-            if search in newList:
-                position = newList.index(search)
-                return [position, (len(newList) - 1) - n]
-            n  += 1
+        prevMap = {} # val -> index
+        
+        for i, n in enumerate(nums):
+            diff = target - n
+            if diff in prevMap:
+                return [prevMap[diff], i]
+            prevMap[n] = i
