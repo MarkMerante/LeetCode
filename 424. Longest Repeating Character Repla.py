497dc8129f5fffe65e4175879ea46c8e95755e0a
MarkMerante <57273536+MarkMerante@users.noreply.github.com>
2022-06-16 20:58:19 -0400

    Update 424. Longest Repeating Character Repla.py

--- 424. Longest Repeating Character Repla.py
+++ 424. Longest Repeating Character Repla.py
@@ -5,12 +5,17 @@
 
 class Solution:
     def characterReplacement(self, s: str, k: int) -> int:
-        charSet = set()
-        maxChar = 0
-        left, right = 0, 1
+        count = {}
+        res = 0
         
-        while right < len(s):
-            if len(charSet) > k:
-                left += 1
-        # To be continued
+        l = 0
+        for r in range(len(s)):
+            count[s[r]] = 1 + count.get(s[r], 0)
+            
+            if (r - l + 1) - max(count.values()) > k:
+                count[s[l]] -= 1
+                l += 1
+            
+            res = max(res, r - l + 1)
+        return res
             
