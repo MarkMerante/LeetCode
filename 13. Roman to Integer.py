fdeb8b19fa6b82c087614a158886aac2547cccd1
MarkMerante <57273536+MarkMerante@users.noreply.github.com>
2022-06-29 21:51:48 -0400

    Add files via upload

---
+++ 13. Roman to Integer.py
@@ -0,0 +1,18 @@
+# 13. Roman to Integer
+
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        romanDict = {"I":1, "IV":4,  "V":5, "IX":9, "X":10, "XL":40, "L":50, 
+                     "XC":90, "C":100, "CD": 400, "D":500, "CM":900, "M":1000}
+        res = 0
+        
+        i = 0
+        while i < len(s):
+            if s[i:i+2] in romanDict:
+                res += romanDict[s[i:i+2]]
+                i += 2
+            else:
+                res += romanDict[s[i]]
+                i +=1
+        
+        return res
